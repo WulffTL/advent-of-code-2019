ecdd5c05f9803c743d8721c063d56f10ec119aeb
Travis Wulff <wulfftl@gmail.com>
2019-12-01 13:49:16 -0800

    add solution for day 1; rocket equation

---
+++ AdventOfCode.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace advent_of_code_2019
+{
+    class AdventOfCode
+    {
+        static void Main(string[] args)
+        {
+            var day = 1;
+            if(args.Length > 1)
+            {
+                day = Int32.Parse(args[1]);
+            }
+
+            switch(day)
+            {
+                case 1:
+                    RocketEquation.PrintTotalFuelRequiredForAllModules();
+                    break;
+                default:
+                    Console.WriteLine("Unexpected input failure");
+                    break;
+            }
+        }
+    }
+}
