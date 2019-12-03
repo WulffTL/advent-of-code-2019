0efd3df9276befb2e0a1c5755b325c281ecc1603
Travis Wulff <wulfftl@gmail.com>
2019-12-02 23:07:38 -0800

    add solution for day 3; crossed wires

---
+++ day-03-crossed-wires/Movement.cs
@@ -0,0 +1,11 @@
+namespace advent_of_code_2019
+{
+    class Movement
+    {
+        private int distance;
+        private Direction direction;
+
+        public int Distance { get => distance; set => distance = value; }
+        public Direction Direction { get => direction; set => direction = value; }
+    }
+}
