e0d7d3f30fb6a586aedb46f237820aa5556689fc
Travis Wulff <WulffTL@users.noreply.github.com>
2019-12-05 19:56:34 -0800

    add solution for day 5; intcode continued (#6)

--- day-02-1202-program-alarm/IntcodeProgram.cs
+++ day-02-1202-program-alarm/IntcodeProgram.cs
@@ -1,89 +1,209 @@
 using System;
+using System.Collections.Generic;
 
 namespace advent_of_code_2019
 {
     class IntcodeProgram
     {
-        public static void PrintAnswer()
+        int[] instructions;
+        int input;
+
+        public IntcodeProgram(int input)
         {
-           var list = GetListFromInput(); 
+            this.instructions = GetListFromInput();
+            this.input = input;
+        }
+        public void PrintAnswer()
+        {
            for(int i = 0; i < 100; i++)
            {
                for(int j = 0; j < 100; j++)
                {
-                    list = Alter1202ProgramAlaram(list, i, j);
-                    list = PerformIntCodes(list);
-                    if(list[0] == 19690720)
+                    Alter1202ProgramAlaram(i, j);
+                    PerformIntCodes();
+                    if(this.instructions[0] == 19690720)
                     {
                         Console.WriteLine($"ANSWER: {i}, {j}");
                         return;
                     }
-                    list = GetListFromInput();
+                    GetListFromInput();
                }
            }
         }
 
-        private static int[] Alter1202ProgramAlaram(int[] list, int val1, int val2)
+        public void Test()
         {
-            list[1] = val1;
-            list[2] = val2;
-            return list;
+            PerformIntCodes();
+        }
+        private void Alter1202ProgramAlaram(int val1, int val2)
+        {
+            this.instructions[1] = val1;
+            this.instructions[2] = val2;
         }
 
-        private static int[] PerformIntCodes(int[] list)
+        private void PerformIntCodes()
         {
             var index = 0;
-            while (index <= list.Length - 1 && list[index] != 99)
+            while (index <= this.instructions.Length - 1 && this.instructions[index] != 99)
             {
-                list = PerformOperation(list, index);
-                index += 4;
+                var pointerMovementlength = PerformOperation(index);
+                index += pointerMovementlength + 1;
             }
-            return list;
         }
 
-        private static int[] PerformOperation(int[] list, int index)
+        private int PerformOperation(int index)
         {
-            if(index > list.Length - 1)
+            if(index > this.instructions.Length - 1)
             {
-                return list;
+                return 0;
             }
 
-            var operation = list[index];
+            var optCode = this.instructions[index];
+            var operation = GetOperation(optCode);
+            var modes = GetModes(optCode);
 
             switch(operation)
             {
                 case 1:
-                    return Add(list, index);
+                    return Add(index, modes);
                 case 2:
-                    return Product(list, index);
+                    return Product(index, modes);
+                case 3:
+                    return PlaceInput(index);
+                case 4:
+                    return Output(index, modes);
+                case 5:
+                    return JumpIfTrue(index, modes);
+                case 6:
+                    return JumpIfFalse(index, modes);
+                case 7:
+                    return IsLessThan(index, modes);
+                case 8:
+                    return IsEquals(index, modes);
                 case 99:
-                    return list;
+                    return 0;
                 default:
-                    throw new Exception("Invalid operator");
+                    throw new Exception($"Invalid operator: {operation}");
             }
         }
 
-        private static int[] Add(int[] list, int startIndex)
+        private int[] GetModes(int optCode)
         {
-            var summandIndex1 = list[startIndex + 1];
-            var summandIndex2 = list[startIndex + 2];
-            var resultIndex = list[startIndex + 3];
-            list[resultIndex] = list[summandIndex1] + list[summandIndex2];
-            return list;
+            int[] modes = new int[3];
+            optCode /= 100;
+            int i = 0;
+            while(optCode > 0)
+            {
+                modes[i] = optCode % 10;
+                optCode /= 10;
+                i++;
+            }
+            return modes;
         }
 
-        private static int[] Product(int[] list, int startIndex)
+        private int GetOperation(int optCode)
         {
-            var factorIndex1 = list[startIndex + 1];
-            var factorIndex2 = list[startIndex + 2];
-            var resultIndex = list[startIndex + 3];
-            list[resultIndex] = list[factorIndex1] * list[factorIndex2];
-            return list;
+            return optCode % 100;
         }
 
-        private static int[] GetListFromInput()
+        private int JumpIfTrue(int index, int[] modes)
         {
-            var text = System.IO.File.ReadAllText(@"day-02-1202-program-alarm/input.txt");
+            var firstParam = this.instructions[index + 1];
+            var secondParam = this.instructions[index + 2];
+
+            var factor1 = modes[0] == 0 ? this.instructions[firstParam] : firstParam;
+            var factor2 = modes[1] == 0 ? this.instructions[secondParam] : secondParam;
+
+            if (factor1 != 0)
+            {
+                return factor2 - index - 1;
+            }
+            return 2;
+        }
+
+        private int JumpIfFalse(int index, int[] modes)
+        {
+            var firstParam = this.instructions[index + 1];
+            var secondParam = this.instructions[index + 2];
+
+            var factor1 = modes[0] == 0 ? this.instructions[firstParam] : firstParam;
+            var factor2 = modes[1] == 0 ? this.instructions[secondParam] : secondParam;
+
+            if (factor1 == 0)
+            {
+                return factor2 - index - 1;
+            }
+            return 2;
+        }
+
+        private int IsLessThan(int index, int[] modes)
+        {
+            var firstParam = this.instructions[index + 1];
+            var secondParam = this.instructions[index + 2];
+            var thirdParam = this.instructions[index + 3];
+
+            var factor1 = modes[0] == 0 ? this.instructions[firstParam] : firstParam;
+            var factor2 = modes[1] == 0 ? this.instructions[secondParam] : secondParam;
+            this.instructions[thirdParam] = factor1 < factor2 ? 1 : 0;
+            return 3;
+        }
+
+        private int IsEquals(int index, int[] modes)
+        {
+            var firstParam = this.instructions[index + 1];
+            var secondParam = this.instructions[index + 2];
+            var thirdParam = this.instructions[index + 3];
+
+            var factor1 = modes[0] == 0 ? this.instructions[firstParam] : firstParam;
+            var factor2 = modes[1] == 0 ? this.instructions[secondParam] : secondParam;
+            this.instructions[thirdParam] = factor1 == factor2 ? 1 : 0;
+            return 3;
+        }
+
+        private int Output(int index, int[] modes)
+        {
+            var outputIndex = this.instructions[index + 1];
+            var factor1 = modes[0] == 0 ? this.instructions[outputIndex] : outputIndex;
+            Console.WriteLine(factor1);
+            return 1;
+        }
+
+        private int PlaceInput(int startIndex)
+        {
+            var newIndex = this.instructions[startIndex + 1];
+            this.instructions[newIndex] = this.input;
+            return 1;
+        }
+
+        private int Add(int startIndex, int[] modes)
+        {
+            var summandIndex1 = this.instructions[startIndex + 1];
+            var summandIndex2 = this.instructions[startIndex + 2];
+            var resultIndex = this.instructions[startIndex + 3];
+
+            var summand1 = modes[0] == 0 ? this.instructions[summandIndex1] : summandIndex1;
+            var summand2 = modes[1] == 0 ? this.instructions[summandIndex2] : summandIndex2;
+            this.instructions[resultIndex] = summand1 + summand2;
+
+            return 3;
+        }
+
+        private int Product(int startIndex, int[] modes)
+        {
+            var factorIndex1 = this.instructions[startIndex + 1];
+            var factorIndex2 = this.instructions[startIndex + 2];
+            var resultIndex = this.instructions[startIndex + 3];
+
+            var factor1 = modes[0] == 0 ? this.instructions[factorIndex1] : factorIndex1;
+            var factor2 = modes[1] == 0 ? this.instructions[factorIndex2] : factorIndex2;
+            this.instructions[resultIndex] = factor1 * factor2;
+
+            return 3;
+        }
+
+        private int[] GetListFromInput()
+        {
+            var text = System.IO.File.ReadAllText(@"day-05-sunny-with-a-chance-of-asteroids/input.txt");
             var list = text.Split(",");
             var intList = new int[list.Length];
             for(int i = 0; i < intList.Length; i++)
